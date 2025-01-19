ef885bf4aba737d57817144b29959d459d91d724
Brian Aston <astonb.dev@gmail.com>
2025-01-19 11:25:24 -0800

    Adding book objects and covers to the information passed to the display component

--- book-app/src/Book.tsx
+++ book-app/src/Book.tsx
@@ -1,10 +1,27 @@
 import './Book.css'
 
-export default function Book({ name = "default"}){
-  console.log("Book", name)
-  return(
+const BASE_IMG_URL = `https://covers.openlibrary.org/b/isbn/`;
+
+export type tBook = {
+  authorName: string,
+  isbn: string[]
+  title: string
+}
+
+export default function Book({ book }: { book: tBook }) {
+  console.log("Book", book)
+  return (
     <div className="book">
-      {name}
+      {book.title}
+      {book.authorName}
+      {book.isbn && 
+        <img src={`${BASE_IMG_URL}${book.isbn[0]}-S.jpg`} />
+      }
+      {book.isbn &&
+        <a href={`https://openlibrary.org/isbn/${book.isbn[0]}`}>
+          Open Library Entry
+        </a>
+      }
     </div>
   )
 }
--- book-app/src/BookContainer.tsx
+++ book-app/src/BookContainer.tsx
@@ -1,23 +1,32 @@
-import { useState } from 'react';
 import Book from "./Book";
-import {getBooks} from "./api.ts";
+import { getBooks } from "./api.ts";
 
 import { useQuery } from '@tanstack/react-query'
 
 export default function BookContainer() {
   const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'], 
-    queryFn: ()=> getBooks()
+    queryKey: ['repoData'],
+    queryFn: () => getBooks()
   });
 
-  if(isLoading) return <p>"Loading..."</p>;
-  if(error) return <p>Error: {error.message}</p>;
+  if (isLoading) return <p>"Loading..."</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   console.log(`data: ${data.numFound}`);
+  const books = data.docs.map((b: any) => {
+    const book = {
+      authorName: b.author_name,
+      isbn: b.isbn,
+      title: b.title,
+    }
+
+    return <Book book={book}/>
+  });
 
   return (
     <div className="book-base-container">
       <div className="book-container">
+        {books}
       </div>
     </div>
   );
